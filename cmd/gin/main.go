1c2b14e8d615aad3b03f35609ee2784b54236e58
xiaotongtong <49009613+thorraythorray@users.noreply.github.com>
2023-07-26 16:23:35 +0800

    fix

--- cmd/gin/main.go
+++ cmd/gin/main.go
@@ -13,10 +13,12 @@ func main() {
 	// 读取配置参数
 	gin.SetMode(global.Mode)
 	global.Logger.Infof("正使用%s环境,配置文件%s", global.Mode, global.Confile)
+
 	engine := gin.Default()
+	// 初始化app
 	ginx.InitMoudles(engine)
+
 	engine.Run(
 		fmt.Sprintf("%s:%s", global.Config.Server.Host, global.Config.Server.Port),
 	)
-
 }
